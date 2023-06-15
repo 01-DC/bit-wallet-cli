6778a42742dc72a6b55d6d2a3d272f7ac59b6b8f
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-06-15 14:35:54 +0530

    added yargs package to manage cli arguments

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+const args = require("yargs").argv
+const sqlite3 = require("sqlite3")
 
+// initiate db connection
+const db = new sqlite3.Database(
+	"./wallets.sqlite",
+	sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,
+	(err) => {
+		if (err) {
+			console.log(err)
+		} else {
+			console.log("Connected to the database")
+		}
+	}
+)
 
+db.run(
+	"CREATE TABLE IF NOT EXISTS wallets (id INTEGER PRIMARY KEY, mnemonic TEXT, address TEXT, used_add INTEGER Default 1)"
+)
 
+console.log(args)
