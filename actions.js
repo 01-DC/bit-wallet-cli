338bc75e2bd64170077912ce5c24e18709aade90
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-06-15 13:07:29 +0530

    added function to import wallet

--- actions.js
+++ actions.js
@@ -20,4 +20,21 @@ export const createWallet = async () => {
 	console.log(`Mnemonic: ${mnemonic}`)
 }
 
-
+export const importWallet = async(mnemonic) => {
+	if (!bip39.validateMnemonic(mnemonic)) {
+		console.log("Invalid mnemonic!")
+		return
+	}
+	const seed = await bip39.mnemonicToSeed(mnemonic)
+	const root = hdkey.fromMasterSeed(seed, bitcoin.networks.testnet)
+	const addrNode = root.derive("m/44'/1'/0'/0/0") // m/44'/0'/0'/0/0 for mainnet
+	const address = bitcoin.payments.p2pkh({
+		pubkey: addrNode.publicKey,
+		network: bitcoin.networks.testnet,
+	}).address
+	await db.run("INSERT INTO wallets (mnemonic, address) VALUES (?, ?)", [
+		mnemonic,
+		address,
+	])
+	console.log(`Imported Wallet Address: ${address}`)
+}
