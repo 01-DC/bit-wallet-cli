f9f736e23e971dee091015d9bd3a929347e8c8be
Devanshu Chatterjee <devanshu4190@gmail.com>
2023-06-15 15:23:29 +0530

    moved db connection to main file & tested some commands

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const args = require("yargs").argv
 const sqlite3 = require("sqlite3")
+const actions = require("./actions")
 
-// initiate db connection
 const db = new sqlite3.Database(
 	"./wallets.sqlite",
 	sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE | sqlite3.OPEN_FULLMUTEX,
 	(err) => {
 		if (err) {
 			console.log(err)
-		} else {
-			console.log("Connected to the database")
 		}
 	}
 )
@@ -18,4 +16,10 @@ db.run(
 	"CREATE TABLE IF NOT EXISTS wallets (id INTEGER PRIMARY KEY, mnemonic TEXT, address TEXT, used_add INTEGER Default 1)"
 )
 
-console.log(args)
+// console.log(args)
+command = args._[0]
+if (command == "create") actions.createWallet(db)
+else if (command == "import") {
+	mnemonic_str = args["m"]
+	actions.importWallet(db, mnemonic_str)
+} else if (command == "list") actions.listWallets(db)
